2f8e5fef2f95d00c694cf3bff84d631f92441154
Eddie-Ward <edwardliew1998@gmail.com>
2022-09-07 17:18:12 -0700

    Define Tile class and select DOM in script

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log("Hello World");
+const START = { y: 0, x: 0 };
+const STATUSES = {
+    TIME: 0,
+    HP: 1,
+    SCORE: 2,
+};
+class Tile {
+    _pos;
+    _content;
+    _value;
+    constructor(_pos, _content, _value) {
+        this._pos = _pos;
+        this._content = _content;
+        this._value = _value;
+    }
+    get pos() {
+        return this._pos;
+    }
+    get content() {
+        return this._content;
+    }
+    get value() {
+        return this._value;
+    }
+}
+const statusesEl = document.querySelectorAll(".js-status");
+// console.log("statusesEl", statusesEl);
+const gridEl = document.querySelector(".js-grid");
+// console.log("gridEl", gridEl);
+const btnsGameEl = document.querySelector(".js-game-btns");
+// console.log("btnsGameEl", btnsGameEl);
+const btnsModeEl = document.querySelector(".js-mode-btns");
+// console.log("btnsModeEl", btnsModeEl);
--- TS/script.ts
+++ TS/script.ts
@@ -1,2 +1,35 @@
-console.log("Hello World");
-console.log("Extra Stuff");
+type Coord = {
+	y: number;
+	x: number;
+};
+
+type Content = "potion" | "enemy" | "start" | "finish";
+
+const START: Coord = { y: 0, x: 0 };
+const STATUSES = {
+	TIME: 0,
+	HP: 1,
+	SCORE: 2,
+};
+
+class Tile {
+	constructor(protected _pos: Coord, protected _content: Content, protected _value: number) {}
+	get pos() {
+		return this._pos;
+	}
+	get content() {
+		return this._content;
+	}
+	get value() {
+		return this._value;
+	}
+}
+
+const statusesEl = document.querySelectorAll(".js-status") as NodeListOf<HTMLDivElement>;
+// console.log("statusesEl", statusesEl);
+const gridEl = document.querySelector(".js-grid") as HTMLDivElement;
+// console.log("gridEl", gridEl);
+const btnsGameEl = document.querySelector(".js-game-btns");
+// console.log("btnsGameEl", btnsGameEl);
+const btnsModeEl = document.querySelector(".js-mode-btns");
+// console.log("btnsModeEl", btnsModeEl);
